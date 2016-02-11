a45d553b7c1c6226338cdd9757f4d76b9145ec41
Brent Bingham <bvbgrad@gmail.com>
2016-02-11 04:10:02 -0600

    Add log4j2 console log only

--- src/org/ll4/test/bvb01/controllers/bvb01Controller.java
+++ src/org/ll4/test/bvb01/controllers/bvb01Controller.java
@@ -4,6 +4,8 @@ import java.text.DateFormat;
 import java.util.Date;
 import java.util.Locale;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,9 +14,12 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 @RequestMapping("/")
 public class bvb01Controller {
- 
+	private static final Logger logger = LogManager.getLogger();
+
     @RequestMapping(method = RequestMethod.GET)
     public String sayHello(Locale locale, ModelMap model) {
+		logger.info("sayHello {}", locale);
+
     	String sGreet = "Hello from bvb01. The client locale is '" + locale.toString() + "'";
         model.addAttribute("greeting", sGreet);
         
@@ -24,11 +29,15 @@ public class bvb01Controller {
 		String formattedDate = dateFormat.format(date);
 		model.addAttribute("serverTime", formattedDate );
 		
+		logger.info("sayHello: {}", sGreet);
+
         return "welcome";
     }
  
     @RequestMapping(value = "/helloagain", method = RequestMethod.GET)
     public String sayHelloAgain(ModelMap model) {
+    	logger.info("sayHelloAgain");
+    	
         model.addAttribute("greeting", "Hello from bvb01 Again, from Spring 4 MVC");
         return "welcome";
     }
